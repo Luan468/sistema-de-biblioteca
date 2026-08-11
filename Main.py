d90861a2e3df42af442ac3b24c69c4301eefe7df
Luan468 <0177462@sesi-es.com>
2026-08-11 07:56:58 -0300

    atualização

--- Main.py
+++ Main.py
@@ -28,3 +28,68 @@ def carregar_livros():
         pass
 
     return livros
+# Salvar os livros
+def salvar_livros(livros):
+    arquivo = open(ARQUIVO, "w", encoding="utf-8")
+
+    for livro in livros:
+        linha = (
+            livro["titulo"] + ";" +
+            livro["autor"] + ";" +
+            livro["ano"] + ";" +
+            livro["codigo"] + ";" +
+            livro["status"] + "\n"
+        )
+
+        arquivo.write(linha)
+
+    arquivo.close()
+
+
+# Cadastrar livro
+def cadastrar(livros):
+    print("\nCadastro de Livro")
+
+    titulo = input("Título: ")
+    autor = input("Autor: ")
+    ano = input("Ano de publicação: ")
+    codigo = input("Código/ISBN: ")
+
+    livro = {
+        "titulo": titulo,
+        "autor": autor,
+        "ano": ano,
+        "codigo": codigo,
+        "status": "Disponível"
+    }
+
+    livros.append(livro)
+
+    salvar_livros(livros)
+
+    print("Livro cadastrado com sucesso!")
+
+
+# Emprestar livro
+def emprestar(livros):
+    codigo = input("Digite o código do livro: ")
+
+    encontrado = False
+
+    for livro in livros:
+        if livro["codigo"] == codigo:
+            encontrado = True
+
+            if livro["status"] == "Disponível":
+                livro["status"] = "Emprestado"
+
+                salvar_livros(livros)
+
+                print("Livro emprestado com sucesso!")
+            else:
+                print("Esse livro já está emprestado.")
+
+            break
+
+    if not encontrado:
+        print("Livro não encontrado.")
